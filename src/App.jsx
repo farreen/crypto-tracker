950f6a1fe15d184da126a9443a6f0e0819faa142
Farah Nasreen <farahnasreen2000@gmail.com>
2023-07-17 21:23:26 +0530

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CoinListing from './components/CoinListing';
-import CoinDetails from './components/CoinDetails';
+import CoinListing from './pages/CoinListing';
+import CoinDetails from './pages/CoinDetails';
 import Header from './components/Header'
 import "./App.css"
 function App() {
 
   return (
-    // <div className='App'>
-    //   <Router>
-    //     <Header/>
-    //     <Routes>
-    //     <Route path='/' element={<CoinListing/>}/>
-    //     <Route path='/details/:id' element={<CoinDetails/>}/>
-    //     </Routes>
-    //   </Router>
-    // </div>
-
     <Router>
       <div className='App'>
         <Header />
