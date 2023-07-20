5c33b753dc4cebb1741cb66f3ff1acef15de8b7f
Farah Nasreen <farahnasreen2000@gmail.com>
2023-07-21 00:55:14 +0530

    Add login, logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,30 +10,41 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
+import { Button } from "@mui/material";
+
 
 
 const Header = () => {
   const navigate = useNavigate();
-  const {currency, setCurrency} = CryptoState();
+  const { currency, setCurrency } = CryptoState();
+  const [isuserLoggedIn, setLoggedIn] = useState(localStorage.getItem("user_credentials") || false);
+
   console.log("currency", currency)
   return (
     <AppBar color="transparent" position="static">
       <Container>
         <Toolbar>
-          <Typography 
-          style={styles.title}
-            onClick={() => {navigate('/')}}
-          > 
+          <Typography
+            style={styles.title}
+            onClick={() => { navigate('/') }}
+          >
             Crypto Tracker
           </Typography>
-          <Select 
-          style={{width: 100, height: 40, color: "#FFFFFF"}}
-          value={currency}
-          onChange={(e) => setCurrency(e.target.value)}
+          <Select
+            style={{ width: 100, height: 40, color: "#FFFFFF" }}
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
           >
             <MenuItem value='USD'>USD</MenuItem>
             <MenuItem value="INR">INR</MenuItem>
           </Select>
+          {
+            isuserLoggedIn ? (
+              <Button onClick={() => {localStorage.clear(); navigate(`/login`)}}type="btn-primary" style={{ border: "0.5px solid black", color: "white", marginLeft: "2px" }}>Log out</Button>
+            ) : (
+              <Button onClick={() => { navigate(`/login`) }} type="btn-primary" style={{ border: "0.5px solid black", color: "white", marginLeft: "2px" }}>Sign Up</Button>
+            )
+          }
         </Toolbar>
       </Container>
     </AppBar>
@@ -44,7 +55,7 @@ export default Header;
 
 
 const styles = {
-  title:{
+  title: {
     flex: 1,
     color: "gold",
     fontFamily: "Montserrat",
